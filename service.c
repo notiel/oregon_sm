7d53100dafe285ccfe03f7f11b6ad7269edd9ac5
Artyrm <ksotar@yandex.ru>
2019-07-01 09:39:13 +0300

    Actual keys.

--- service.c
+++ service.c
@@ -10,7 +10,7 @@ const KeyStroke KeyStrokes[]= {
 {    PILL_RESET_SIG,              "PILL_RESET",                '0'    },
 {    PILL_GHOUL_SIG,              "PILL_GHOUL",                'g'    },
 {    PILL_TEST_SIG,               "PILL_TEST",                 't'    },
-{    PILL_REMOVED_SIG,            "PILL_REMOVED",              'r'    },
+{    PILL_REMOVED_SIG,            "PILL_REMOVED",              'q'    },
 {    AGONY_SIG,                   "PILL_AGONY",                'a'    },
 {    BLESSED_SIG,                 "BLESSED",                   'b'    },
 {    IMMUNE_SIG,                  "IMMUNE",                    'i'    },
@@ -18,6 +18,15 @@ const KeyStroke KeyStrokes[]= {
 {    TIME_TICK_1S_SIG,            "TIME_TICK_1S",              's'    },
 {    TIME_TICK_10S_SIG,           "TIME_TICK_10S",             'd'    },
 {    TIME_TICK_1M_SIG,            "TIME_TICK_1M",              'm'    },
+
+{	PILL_ANY_SIG, 	"	PILL_ANY	"	,	 'p'	},
+{	PILL_HEAL_SIG, 	"	PILL_HEAL	"	,	 '1'	},
+{	PILL_RAD_X_SIG, 	"	PILL_RAD_X	"	,	 '3'	},
+{	PILL_BLESS_SIG, 	"	PILL_BLESS	"	,	 '6'	},
+{	PILL_CURSE_SIG, 	"	PILL_CURSE	"	,	 '5'	},
+{	PILL_ANTIRAD_SIG, 	"	PILL_ANTIRAD	"	,	 '4'	},
+{	PILL_HEALSTATION_SIG, 	"	PILL_HEALSTATION	"	,	 '2'	},
+
 
                 
 {    TERMINATE_SIG,               "TERMINATE",                 0x1B   }
